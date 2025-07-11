f60af724fb71a75199bbac6ca9b57cf408c9ecbf
Ben Potter <bp@benpot.com>
2025-07-11 13:04:23 -0400

    refactor into structures

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,176 @@
+use std::io::{BufRead, Lines};
+use std::str::FromStr;
+
 #[derive(Debug)]
 pub enum Error {
     InvalidRow(String),
-    InvalidColumn(String),
+    FailedRead(String),
 }
 
-/// Returns the index of a bin for a data point `x` between `min` and `max` inclusive.
-pub fn get_bin_index(x: f32, min: f32, max: f32, bins: usize) -> usize {
-    ((x - min) / (max.next_up() - min) * bins as f32).trunc() as usize
+// struct ColumnParser<T, Reader>
+// - T must implement FromStr for ::parse<T>()
+// - Reader must implement Read for reading rows
+// - Takes:
+//   - reader: Read (may be a Stdin or a BufReader<File>)
+//   - column: usize
+//   - delim: &str
+// - Handle parsing into T using ::parse<T>()
+// - Provides iterator over rows via .rows()
+// - Columns are 0-indexed
+
+pub struct ColumnParser<T, Reader> {
+    lines: Lines<Reader>,
+    column: usize,
+    delim: String,
+    _phantom: std::marker::PhantomData<T>,
 }
 
-/// Returns the midpoint of the bin interval for a given index.
-pub fn get_bin_label(index: usize, min: f32, max: f32, bins: usize) -> f32 {
-    let bin_width = (max - min) / bins as f32;
-    index as f32 * bin_width + min + bin_width / 2.0
+impl<T: FromStr, Reader: BufRead> ColumnParser<T, Reader> {
+    pub fn new(reader: Reader, column: usize, delim: &str) -> Self {
+        Self {
+            lines: reader.lines(),
+            column,
+            delim: delim.to_string(),
+            _phantom: std::marker::PhantomData,
+        }
+    }
+
+    pub fn parse_row(&mut self) -> Option<Result<Option<T>, Error>> {
+        // Get next line from BufRead.
+        // If it returns None, there are no more lines to read.
+        match self.lines.next()? {
+            Ok(line) => {
+                // Split the line by delim and return the nth column.
+                match line.split(&self.delim).nth(self.column) {
+                    Some(value) => Some(Ok(value.parse::<T>().ok())),
+
+                    // Input is malformed or column is out of bounds.
+                    None => Some(Err(Error::InvalidRow(format!(
+                        "column {} does not exist in row",
+                        self.column
+                    )))),
+                }
+            }
+
+            // If next() returns an Err, then reading input failed.
+            Err(e) => Some(Err(Error::FailedRead(format!(
+                "failed to read line: {}",
+                e
+            )))),
+        }
+    }
+
+    pub fn rows(self) -> Rows<T, Reader> {
+        Rows { parser: self }
+    }
 }
 
-/// Return the f32 at `column` delimited by `delim` in `buffer`.
-pub fn get_data_at_column(buffer: &String, column: usize, delim: &str) -> Result<f32, Error> {
-    let col_index = column.checked_sub(1).ok_or(Error::InvalidColumn(format!(
-        "first column index is 1 but got {}",
-        column
-    )))?;
+pub struct Rows<T, Reader> {
+    parser: ColumnParser<T, Reader>,
+}
 
-    let data = buffer
-        .split(delim)
-        .nth(col_index)
-        .ok_or(Error::InvalidColumn(format!(
-            "column {} out of bounds",
-            column
-        )))?;
+impl<T: FromStr, Reader: BufRead> Iterator for Rows<T, Reader> {
+    type Item = Result<Option<T>, Error>;
 
-    data.parse()
-        .map_err(|e| Error::InvalidRow(format!("failed to parse f32 from {}: {}", data, e)))
+    fn next(&mut self) -> Option<Self::Item> {
+        self.parser.parse_row()
+    }
+}
+
+// struct Histogram<T>
+// - Handles binning data on creation
+//   - fn from_values(values: Vec<T>, num_bins: usize) -> Self
+// - Can't be constructed from only an iterator
+//   - Need max and min
+//   - Need num_bins
+// - Bins:
+//   - Label
+//   - Count
+// - Can be converted to UTF-8:
+//   - Into csv
+//   - Into space delim
+// - Can this be plotted?
+
+pub struct Bin {
+    pub label: f32,
+    pub count: usize,
+}
+
+pub struct Histogram {
+    bins: Vec<Bin>,
+}
+
+impl Histogram {
+    pub fn from_values(values: Vec<f32>, num_bins: usize) -> Self {
+        let bins = match values
+            .iter()
+            .fold(None, |acc: Option<(f32, f32)>, &x| match acc {
+                Some((min, max)) => Some((min.min(x), max.max(x))),
+                None => Some((f32::INFINITY, f32::NEG_INFINITY)),
+            }) {
+            Some((min, max)) => {
+                let bin_width = (max - min) / num_bins as f32;
+                let mut bins: Vec<Bin> = (0..num_bins)
+                    .into_iter()
+                    .map(|i| i as f32 * bin_width + min + bin_width / 2.0)
+                    .map(|label| Bin { label, count: 0 })
+                    .collect();
+
+                values
+                    .iter()
+                    .map(|&x| get_bin_index(x, min, max, num_bins))
+                    .for_each(|i| bins[i].count += 1);
+
+                bins
+            }
+            None => Vec::new(),
+        };
+
+        Histogram { bins }
+    }
+
+    pub fn into_bins(self) -> Vec<Bin> {
+        self.bins
+    }
+
+    pub fn into_counts(self) -> Vec<usize> {
+        self.bins.into_iter().map(|bin| bin.count).collect()
+    }
+
+    pub fn into_labels(self) -> Vec<f32> {
+        self.bins.into_iter().map(|bin| bin.label).collect()
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io::Cursor;
 
     #[test]
-    fn test_get_bin_index() {
-        assert_eq!(get_bin_index(0.0, 0.0, 10.0, 10), 0);
-        assert_eq!(get_bin_index(9.9, 0.0, 10.0, 10), 9);
-        assert_eq!(get_bin_index(5.0, 0.0, 10.0, 10), 4);
+    fn test_column_parser() {
+        let data = "1.0,2.0,3.0\n4.0,5.0,6.0\n7.0,8.0,9.0\n";
+        let cursor = Cursor::new(data);
 
-        assert_eq!(get_bin_index(-1.0, -1.0, 1.0, 20), 0);
-        assert_eq!(get_bin_index(0.9, -1.0, 1.0, 20), 19);
+        let result: Vec<f32> = ColumnParser::<f32, _>::new(cursor, 1, ",")
+            .rows()
+            .filter_map(|row| row.unwrap())
+            .collect();
+
+        assert_eq!(result, vec![2.0, 5.0, 8.0]);
     }
 
     #[test]
-    fn test_get_bin_label() {
-        assert_eq!(get_bin_label(0, 0.0, 10.0, 10), 0.5);
-        assert_eq!(get_bin_label(9, 0.0, 10.0, 10), 9.5);
-        assert_eq!(get_bin_label(5, 0.0, 10.0, 10), 5.5);
+    fn test_histogram_counts_from_values() {
+        let values = vec![2.0, 1.0, 2.0, 3.0, 3.0, 2.0, 0.0, 1.0, 1.0, 1.0];
+        let histogram = Histogram::from_values(values, 3);
+        assert_eq!(histogram.into_counts(), vec![5, 3, 2]);
+    }
 
-        assert_eq!(get_bin_label(0, -1.0, 1.0, 20), -0.95);
-        assert_eq!(get_bin_label(19, -1.0, 1.0, 20), 0.95);
+    #[test]
+    fn test_histogram_labels_from_values() {
+        let values = vec![2.0, 1.0, 2.0, 3.0, 3.0, 2.0, 0.0, 1.0, 1.0, 1.0];
+        let histogram = Histogram::from_values(values, 3);
+        assert_eq!(histogram.into_labels(), vec![0.5, 1.5, 2.5]);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,44 @@
 use clap::Parser;
-use rhisto::{get_bin_index, get_bin_label, get_data_at_column};
+use rhisto::{ColumnParser, Histogram};
 use std::{
     fs::File,
-    io::{BufRead, BufReader},
+    io::{BufRead, BufReader, BufWriter, Write},
     path::PathBuf,
 };
 
 fn main() {
     let args = Args::parse();
 
-    let data: Vec<f32> = match args.input {
-        Some(path_buf) => {
-            let file = File::open(&path_buf).expect("failed to open input file");
-            let mut reader = BufReader::new(file);
-
-            if args.skip_header {
-                reader.skip_until(b'\n').expect("failed to skip header");
-            }
-
-            // Read column from each line.
-            reader
-                .lines()
-                .map(|line| match line {
-                    Ok(buf) => get_data_at_column(&buf, args.column, &args.delim)
-                        .expect("failed to parse row"),
-                    Err(e) => {
-                        println!("failed to read line: {}", e);
-                        f32::NAN
-                    }
-                })
-                .collect()
-        }
-        // TODO: Support reading from STDIN.
-        None => unimplemented!(),
+    let mut reader: Box<dyn BufRead> = match args.input {
+        Some(path_buf) => Box::new(BufReader::new(
+            File::open(&path_buf).expect("failed to open input file"),
+        )),
+        None => Box::new(BufReader::new(std::io::stdin())),
     };
 
-    // Find the data interval.
-    let (min, max): (f32, f32) = data
-        .iter()
-        .fold(None, |acc: Option<(f32, f32)>, &x| match acc {
-            Some((min, max)) => Some((min.min(x), max.max(x))),
-            None => Some((f32::INFINITY, f32::NEG_INFINITY)),
-        })
-        .expect("no data provided");
-
-    let mut bins: Vec<u32> = vec![0; args.num_bins];
-    for bin in data
-        .iter()
-        .map(|&x| get_bin_index(x, min, max, args.num_bins))
-    {
-        bins[bin] += 1;
+    if args.skip_header {
+        reader.skip_until(b'\n').expect("failed to skip header");
     }
 
-    match args.output {
-        Some(_) => unimplemented!(),
-        None => {
-            println!("bin_label,bin_value");
-            for (i, x) in bins.iter().enumerate() {
-                println!(
-                    "{:0.2}{}{:0.2}",
-                    get_bin_label(i, min, max, args.num_bins),
-                    &args.delim,
-                    x
-                );
-            }
-        }
+    let values: Vec<f32> = ColumnParser::<f32, _>::new(reader, args.column, &args.delim)
+        .rows()
+        // Panic on IO errors.
+        // Discard rows with no value.
+        .filter_map(|row| row.unwrap())
+        .collect();
+
+    let histo = Histogram::from_values(values, args.num_bins);
+
+    let mut writer: Box<dyn Write> = match args.output {
+        Some(path_buf) => Box::new(BufWriter::new(
+            File::create(&path_buf).expect("failed to open output file"),
+        )),
+        None => Box::new(BufWriter::new(std::io::stdout())),
+    };
+
+    let _ = writeln!(writer, "bin_label,bin_value");
+    for bin in histo.into_bins().iter() {
+        let _ = writeln!(writer, "{:0.2}{}{:0.2}", bin.label, &args.delim, bin.count);
     }
 }
 
@@ -75,10 +49,11 @@ struct Args {
     input: Option<PathBuf>,
 
     /// The optional path to write histogram data to.
+    #[arg(short, long)]
     output: Option<PathBuf>,
 
-    /// The column in the input buffer to read.
-    #[arg(short, long, default_value_t = 1)]
+    /// The zero indexed column in the input buffer to read.
+    #[arg(short, long, default_value_t = 0)]
     column: usize,
 
     /// The delimeting pattern used to separate columns in the input.
